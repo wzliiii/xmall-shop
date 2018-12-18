2b281138b58653a90b624b3777e389967cbfd718
wzl <1083305159@qq.com>
2018-12-19 00:05:16 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header height="100px"></el-header>
+      <el-main></el-main>
+      <el-footer height="350px"></el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+  @import "statics/css/index.scss";
+  .el-header {
+    background-color: #040404;
+    color: #333;
+    line-height: 100px;
+  }
+  .el-main {
+    background-color: #ededed;
+    min-height: calc(100vh - 454px);
+    color: #333;
+    text-align: center;
+  }
+
+  .el-footer {
+    margin-top: 60px;
+    background-color: #fafafa;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
